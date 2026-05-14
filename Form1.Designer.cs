ae19c322604675a0e7e70b498058d22e742434ea
anthony2004lp <144872785+anthony2004lp@users.noreply.github.com>
2026-05-14 09:37:24 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace FormulaGaussExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lblBalanza = new System.Windows.Forms.Label();
+            this.txtBalanza = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            //this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.btnLogin = new System.Windows.Forms.ToolStripSplitButton();
+            this.lblUsuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblContraseña = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNGRESARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.conexiónBalanzaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calibraciónBalanzaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.abrirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarConfiguraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtContraseña = new System.Windows.Forms.ToolStripTextBox();
+            this.txtUsuario = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(190, 319);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(228, 40);
+            this.btnRegistrar.TabIndex = 0;
+            this.btnRegistrar.Text = "REGISTRAR PESO";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // lblBalanza
+            // 
+            this.lblBalanza.AutoSize = true;
+            this.lblBalanza.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBalanza.Location = new System.Drawing.Point(9, 82);
+            this.lblBalanza.Name = "lblBalanza";
+            this.lblBalanza.Size = new System.Drawing.Size(642, 73);
+            this.lblBalanza.TabIndex = 1;
+            this.lblBalanza.Text = "PESO DE BALANZA";
+            this.lblBalanza.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBalanza
+            // 
+            this.txtBalanza.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBalanza.Location = new System.Drawing.Point(116, 195);
+            this.txtBalanza.Name = "txtBalanza";
+            this.txtBalanza.Size = new System.Drawing.Size(384, 80);
+            this.txtBalanza.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            //this.toolStripSplitButton1,
+            this.btnLogin,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(663, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripSplitButton1
+            // 
+            //this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            //this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            //this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            //this.toolStripSplitButton1.Size = new System.Drawing.Size(0, 22);
+            //this.toolStripSplitButton1.Text = "LOGIN";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnLogin.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblUsuario,
+            this.lblContraseña,
+            this.iNGRESARToolStripMenuItem});
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(58, 22);
+            this.btnLogin.Text = "LOGIN";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtUsuario});
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(180, 22);
+            this.lblUsuario.Text = "USUARIO";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtContraseña});
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(180, 22);
+            this.lblContraseña.Text = "CONTRASEÑA";
+            // 
+            // iNGRESARToolStripMenuItem
+            // 
+            this.iNGRESARToolStripMenuItem.Name = "iNGRESARToolStripMenuItem";
+            this.iNGRESARToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iNGRESARToolStripMenuItem.Text = "INGRESAR";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.conexiónBalanzaToolStripMenuItem,
+            this.calibraciónBalanzaToolStripMenuItem,
+            this.guardarConfiguraciónToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(54, 22);
+            this.toolStripDropDownButton1.Text = "MENÚ";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // conexiónBalanzaToolStripMenuItem
+            // 
+            this.conexiónBalanzaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.abrirToolStripMenuItem,
+            this.cerrarToolStripMenuItem});
+            this.conexiónBalanzaToolStripMenuItem.Name = "conexiónBalanzaToolStripMenuItem";
+            this.conexiónBalanzaToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.conexiónBalanzaToolStripMenuItem.Text = "Conexión balanza";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            // 
+            // calibraciónBalanzaToolStripMenuItem
+            // 
+            this.calibraciónBalanzaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.abrirToolStripMenuItem1,
+            this.cerrarToolStripMenuItem1});
+            this.calibraciónBalanzaToolStripMenuItem.Name = "calibraciónBalanzaToolStripMenuItem";
+            this.calibraciónBalanzaToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.calibraciónBalanzaToolStripMenuItem.Text = "Calibración balanza";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // abrirToolStripMenuItem1
+            // 
+            this.abrirToolStripMenuItem1.Name = "abrirToolStripMenuItem1";
+            this.abrirToolStripMenuItem1.Size = new System.Drawing.Size(160, 22);
+            this.abrirToolStripMenuItem1.Text = "Abrir";
+            // 
+            // cerrarToolStripMenuItem1
+            // 
+            this.cerrarToolStripMenuItem1.Name = "cerrarToolStripMenuItem1";
+            this.cerrarToolStripMenuItem1.Size = new System.Drawing.Size(160, 22);
+            this.cerrarToolStripMenuItem1.Text = "Cerrar";
+            // 
+            // guardarConfiguraciónToolStripMenuItem
+            // 
+            this.guardarConfiguraciónToolStripMenuItem.Name = "guardarConfiguraciónToolStripMenuItem";
+            this.guardarConfiguraciónToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.guardarConfiguraciónToolStripMenuItem.Text = "Guardar configuración";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 558);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(663, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(132, 17);
+            this.toolStripStatusLabel1.Text = "CONEXCIÓN BALANZA";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(60, 17);
+            this.toolStripStatusLabel2.Text = "BALANZA";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(124, 17);
+            this.toolStripStatusLabel3.Text = "TIEMPO CONECTADO";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(19, 17);
+            this.toolStripStatusLabel4.Text = "1S";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 23);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 23);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 580);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.txtBalanza);
+            this.Controls.Add(this.lblBalanza);
+            this.Controls.Add(this.btnRegistrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Label lblBalanza;
+        private System.Windows.Forms.TextBox txtBalanza;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        //private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripSplitButton btnLogin;
+        private System.Windows.Forms.ToolStripMenuItem lblUsuario;
+        private System.Windows.Forms.ToolStripMenuItem lblContraseña;
+        private System.Windows.Forms.ToolStripMenuItem iNGRESARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem conexiónBalanzaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarConfiguraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calibraciónBalanzaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripTextBox txtUsuario;
+        private System.Windows.Forms.ToolStripTextBox txtContraseña;
+    }
+}
+
